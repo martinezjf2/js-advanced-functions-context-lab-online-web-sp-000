fc79e809c064fb801fda678724c4ed70f54969c3
Jeffrey Martinez <martinezjf2@gmail.com>
2021-06-02 22:10:23 -0400

    Done.

--- index.js
+++ index.js
@@ -37,23 +37,53 @@ function createEmployeeRecords(arys) {
     return arys.map(createEmployeeRecord)
 }
 
-function createDSObj(getType, dateStamp) {
-    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+// function createDSObj(getType, dateStamp) {
+//     return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+// }
+
+function createTimeInEvent(dateStamp) {
+   const [date, hour] = dateStamp.split(" ");
+   this.timeInEvents.push({
+       type:"TimeIn",
+       hour: parseInt(hour, 10),
+       date: date
+   });
+   return this
 }
 
-function createTimeInEvent(obj, dateStamp) {
-    obj.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this
 }
 
-function createTimeOutEvent() {
+function hoursWorkedOnDate(dateStamp) {
+    let timeIn = this.timeInEvents.filter(timeIn => timeIn.date === dateStamp)[0].hour;
+    let timeOut = this.timeOutEvents.filter(timeOut => timeOut.date === dateStamp)[0].hour;
 
+    return ((timeOut - timeIn) * .01);
 }
 
-function wagesEarnedOnDate() {
-
+function wagesEarnedOnDate(dateStamp) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, dateStamp)
+    // What does this mean inside a functiona and what is the functionality of the .call method?
 }
 
-function calculatePayroll() {
+function findEmployeeByFirstName(sourceArray, firstName) {
+    let empRecord = sourceArray.filter(emp => emp.firstName === firstName);
 
+    return !!empRecord[0] ? empRecord[0] : undefined;
+    // why do we invoke the 2 exclamation points in the beginning
+}
+ 
+function calculatePayroll(empArray) {
+    return empArray.reduce((total, wages) => total += allWagesFor.call(wages), 0);
+    // what is the purpose of the call method and why we need to invoke the 0
 }
 
